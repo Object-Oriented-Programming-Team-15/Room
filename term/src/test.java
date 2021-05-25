c570ca5f183e47a17b88a063243de9c731e185c8
donghyeon807 <83694916+donghyeon807@users.noreply.github.com>
2021-05-25 16:23:25 +0900

    Update test.java

--- term/src/test.java
+++ term/src/test.java
@@ -1,10 +1,17 @@
-
 public class test {
 
 	public static void main(String[] args) {
-		MemberRoom a = new MemberRoom(5,3,false);
+		MemberRoom a = new MemberRoom(5,3);
 		
 		a.printRoom();
+		
+		Member m = new Member();
+		
+		a.assignRoom(m);
+		
+		a.printRoom();
+		a.assignRoom(m);
 	}
 
 }
+
