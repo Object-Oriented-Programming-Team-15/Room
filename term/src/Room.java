2ffec56e4073f9c17942ceaedccd9df4eac50743
donghyeon807 <83694916+donghyeon807@users.noreply.github.com>
2021-05-25 16:22:37 +0900

    Update Room.java

--- term/src/Room.java
+++ term/src/Room.java
@@ -1,35 +1,48 @@
 
 public class Room{
 	private int roomNumber;
-	private int capacity; //ÃÖ´ë ÀÎ¿ø
-	private boolean isUsed;
+	private int capacity; //ìµœëŒ€ ì¸ì›
+	private int isUsed;
+	private int assignPeople;
 	
 	public Room()
 	{
 		this.roomNumber = 0;
 		this.capacity = 0;
-		this.isUsed = false;
+		this.isUsed = 0;
+		this.assignPeople = 0;
 	}
 	
-	public Room(int roomNumber, int capacity, boolean isUsed)
+	public Room(int roomNumber, int capacity)
+	{
+		this.roomNumber = roomNumber;
+		this.capacity = capacity;
+		this.isUsed = isUsed;
+		this.assignPeople = 0;
+	}
+	
+	public void setAssignPeople(int id)
+	{
+		this.assignPeople = id;
+	}
+	
+	public void setRoom(int roomNumber, int capacity, int isUsed)
 	{
 		this.roomNumber = roomNumber;
 		this.capacity = capacity;
 		this.isUsed = isUsed;
 	}
 	
-	public void setRoom(int roomNumber, int capacity, boolean isUsed)
-	{
-		this.roomNumber = roomNumber;
-		this.capacity = capacity;
-		this.isUsed = isUsed;
-	}
 	
-	
-	public void setUsed()
+public void setUsedassign()
 	{
-		this.isUsed = true;
+		this.isUsed = 1;
 	}
+
+public void setUsed()
+{
+	this.isUsed = 2;
+}
 	
 	public int getroomNumber()
 	{
@@ -41,9 +54,8 @@ public class Room{
 		return 	capacity;
 	}
 	
-	public boolean getUsed()
+	public int getUsed()
 	{
 		return this.isUsed;
 	}
 }
-
