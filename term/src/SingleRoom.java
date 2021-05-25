5ef740f3313a1ea044d0b828a1f462953dc47c77
donghyeon807 <83694916+donghyeon807@users.noreply.github.com>
2021-05-25 16:23:07 +0900

    Update SingleRoom.java

--- term/src/SingleRoom.java
+++ term/src/SingleRoom.java
@@ -6,9 +6,9 @@ public class SingleRoom extends Room implements Roominter{
 		super();
 	}
 	
-	public SingleRoom(int a, int b, boolean c)
+	public SingleRoom(int a, int b)
 	{
-		super(a,b,c);
+		super(a,b);
 	}
 	
 	@Override
@@ -16,14 +16,29 @@ public class SingleRoom extends Room implements Roominter{
 		System.out.println("This Room is Single Room");
 		System.out.println("Room Number : " + this.getroomNumber());
 		System.out.println("Capacity : " + this.getcapacity());
-		if(this.getUsed() == true)
+		if(this.getUsed() == 1)
+			System.out.println("This room is Assigned");
+		else if(this.getUsed() == 2)
 			System.out.println("This room is Used");
 		else
-			System.out.println("This room is empty");
+			System.out.println("This room is Empty");
 	}
 
 	@Override
-	public void assignRoom() {
-		System.out.println("Member Room Assign");
+	public void assignRoom(Member m) {
+		System.out.println();
+		
+		if(this.getUsed() == 0)
+		{
+			System.out.println(m.getID() + " assign single Room");
+			System.out.println("And Room number is "+this.getroomNumber());
+			System.out.println();
+			this.setAssignPeople(m.getID());
+		}
+		else if (this.getUsed() == 1)
+			System.out.println("Room " +this.getroomNumber()+ " is already Assigned");
+		else
+			System.out.println("Room " +this.getroomNumber()+ " is already Used");
+		this.setUsedassign();
 	}
 }
