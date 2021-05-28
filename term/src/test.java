9454908d7eb1bdc7694ba1d20e7cd4f528698005
donghyeon807 <83694916+donghyeon807@users.noreply.github.com>
2021-05-28 14:13:49 +0900

    Update test.java

--- term/src/test.java
+++ term/src/test.java
@@ -1,17 +1,30 @@
+
 public class test {
 
 	public static void main(String[] args) {
 		MemberRoom a = new MemberRoom(5,3);
+		SingleRoom a2 = new SingleRoom(7,4);
 		
 		a.printRoom();
 		
-		Member m = new Member();
+		Member m = new Member("Kim",1, 123, 1000);
+		Member m2 = new Member("Lim",3,456,2000);
 		
 		a.assignRoom(m);
 		
 		a.printRoom();
 		a.assignRoom(m);
+		
+		a2.RoomUsing(m2.getID());
+		a2.RoomUsing(m2.getID());
+		a2.printRoom();
+		
+		a2.logoutRoom(m2.getID());
+		a2.RoomUsing(m2.getID());
+		a2.logoutRoom(m2.getID());
+		a2.logoutRoom(m2.getID());
+		a2.printRoom();
+		
 	}
 
 }
-
